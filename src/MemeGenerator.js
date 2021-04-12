a2dd8cc9185e763051d96a8acd9280e683ef586e
shanecolby <shanecolbyzero8@gmail.com>
2021-04-12 17:27:33 -0500

    display text from input boxes

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -9,6 +9,7 @@ class MemeGenerator extends Component {
       randomImg: "http://i.imgflip.com/1bij.jpg",
       allMemeImages: []
     }
+    this.handleChange = this.handleChange.bind(this)
   }
 
   componentDidMount() {
@@ -21,9 +22,45 @@ class MemeGenerator extends Component {
       })
   }
 
+  handleChange(event) {
+    const { name, value } = event.target
+    this.setState({
+      [name]: value
+    })
+  }
+
   render() {
     return (
-      
+      <div>
+        <form className="meme-form">
+          <input
+            type="text"
+            name="topText"
+            placeholder="Top Text"
+            value={this.state.topText}
+            onChange={this.handleChange}
+          />
+
+          <input
+            type="text"
+            name="bottomText"
+            placeholder="Top Text"
+            value={this.state.bottomText}
+            onChange={this.handleChange}
+          />
+
+          <button>Gen</button>
+        </form>
+
+        <div className="meme">
+          <img src={this.state.randomImg} alt="" />
+          <h2 className="top">{this.state.topText}</h2>
+          <h2 className="bottom">{this.state.bottomText}</h2>
+
+
+
+        </div>
+      </div >
     )
   }
 }
