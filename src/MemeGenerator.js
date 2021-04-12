e4f797385da4666e6e1576f28ae4a927296fe2fb
shanecolby <shanecolbyzero8@gmail.com>
2021-04-12 17:08:17 -0500

    add state to memegenerator and make call to api

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -3,12 +3,27 @@ import React, { Component } from "react"
 class MemeGenerator extends Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      topText: "",
+      bottomText: "",
+      randomImg: "http://i.imgflip.com/1bij.jpg",
+      allMemeImages: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://api.imgflip.com/get_memes")
+      .then(response => response.json())
+      .then(response => {
+        const { memes } = response.data
+        console.log(memes[0])
+        this.setState({ allMemeImages: memes })
+      })
   }
 
   render() {
     return (
-      <h1>Hello</h1>
+      
     )
   }
 }
