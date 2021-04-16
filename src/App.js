62db617198966d92486afbf4b581a95f5196c004
shanecolby <shanecolbyzero8@gmail.com>
2021-04-16 16:36:07 -0500

    add footer and stlying

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from './Header'
 import MemeGenerator from './MemeGenerator'
 import './style.css'
+import Footer from './Footer'
 
 function App() {
   return (
     <div>
       <Header />
       <MemeGenerator />
+      <Footer />
     </div>
   )
 }
---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+      <img
+        src="https://static.wikia.nocookie.net/meme/images/c/ce/24058219.png/revision/latest?cb=20181024173726"
+        alt="footer_img"
+      />
+      <p>Meme Generator</p>
+
+    </footer>
+  )
+}
+
+export default Footer
