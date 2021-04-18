177987cfa0e222f2c0682c14c99ab0e1fe2c3e6e
shanecolby <shanecolbyzero8@gmail.com>
2021-04-18 08:15:05 -0500

    background color with className round img corners

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -39,7 +39,7 @@ class MemeGenerator extends Component {
 
   render() {
     return (
-      <div>
+      <div className="body">
         <form className="meme-form" onSubmit={this.handleSubmit}>
           <input
             type="text"
